ddc07284f577914ef0b948c19259d2545d3406f0
itsmiashish <ashishind63@gmail.com>
2025-12-05 11:15:06 +0530

    first changes by ashish

---
+++ src/main/java/com/jspiders/project/bms/Ashish.java
@@ -0,0 +1,8 @@
+package com.jspiders.project.bms;
+
+public class Ashish {
+    public static void main(String[] args) {
+        System.out.println("changes done by Ashish 👨‍💻👨‍💻👨‍💻");
+        System.out.println("new changes ");
+    }
+}
